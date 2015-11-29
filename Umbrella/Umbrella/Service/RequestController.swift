b883e2fa4842addc72a1fa9e5be4e2095909bf8b
StijnWillems <stijn.willems@icapps.com>
2015-11-29 16:01:33 +0100

    Retreiving all objects

--- Umbrella/Umbrella/Service/RequestController.swift
+++ Umbrella/Umbrella/Service/RequestController.swift
@@ -18,7 +18,7 @@ public class RequestController {
 		session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
 	}
 	
-	public func saveBody<BodyType: BaseModel, ResponseType: BaseModel>(body: BodyType, completion:(response: ResponseType)->()){
+	public func save<BodyType: BaseModel, ResponseType: BaseModel>(body: BodyType, completion:(response: ResponseType)->()){
 		let request = serviceParameters.request
 		request.HTTPMethod = "POST"
 		
@@ -26,12 +26,24 @@ public class RequestController {
 			request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(bodyObject, options: .PrettyPrinted)
 		}
 		
-		/* Start a new Task */
 		let task = session.dataTaskWithRequest(request, completionHandler: { [unowned self] (data, response, error) -> Void in
 			self.responseController.handleResponse((data: data, urlResponse: response, error: error), completion: completion)
 		})
 		
 		task.resume()
 
+	}
+	
+	public func retrieve<ResponseType: BaseModel>(completion:(response: [ResponseType])->()){
+		let request = serviceParameters.request
+		request.HTTPMethod = "GET"
+		
+		/* Start a new Task */
+		let task = session.dataTaskWithRequest(request, completionHandler: { [unowned self] (data, response, error) -> Void in
+			self.responseController.handleResponse((data: data, urlResponse: response, error: error), completion: completion)
+		})
+		
+		task.resume()
+		
 	}
 }
