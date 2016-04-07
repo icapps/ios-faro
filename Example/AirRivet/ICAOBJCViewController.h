6590a4abc4a57915fc5d1b876df08ac81fb8703d
StijnWillems <stijn.willems@icapps.com>
2016-04-07 11:53:34 +0200

    Added objective-c viewcontroller

---
+++ Example/AirRivet/ICAOBJCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ICAOBJCViewController.h
+//  AirRivet
+//
+//  Created by Stijn Willems on 07/04/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICAOBJCViewController : UIViewController
+
+@end
---
+++ Example/AirRivet/ICAOBJCViewController.m
@@ -0,0 +1,13 @@
+//
+//  ICAOBJCViewController.m
+//  AirRivet
+//
+//  Created by Stijn Willems on 07/04/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import "ICAOBJCViewController.h"
+
+@implementation ICAOBJCViewController
+
+@end
