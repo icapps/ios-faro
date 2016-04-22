e137947cc766a761b5702decf13da8f5f7050cec
Hans Van Herreweghe <ikbenhansvanherreweghe@gmail.com>
2016-04-22 13:24:31 +0200

    Unit testing #17: added ResponseControllerTests

---
+++ Example/Tests/ResponseControllerTests.swift
@@ -0,0 +1,18 @@
+//
+//  ResponseControllerTests.swift
+//  AirRivet
+//
+//  Created by Hans Van Herreweghe on 22/04/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import AirRivet
+
+class ResponseControllerTests: XCTestCase {
+    
+    func testExample() {
+        XCTFail()
+    }
+    
+}
