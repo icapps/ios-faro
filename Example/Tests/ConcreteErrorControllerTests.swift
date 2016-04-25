91ee485eaca3baa331d2e7ebe2634e9a0a62e894
StijnWillems <stijn.willems@icapps.com>
2016-04-25 11:57:34 +0200

    Made it nimble tests because they have a good syntax for testing throws

--- Example/Tests/ConcreteErrorControllerTests.swift
+++ Example/Tests/ConcreteErrorControllerTests.swift
@@ -6,69 +6,58 @@
 //  Copyright © 2016 CocoaPods. All rights reserved.
 //
 
+import Nimble
+import Quick
 import XCTest
 @testable import AirRivet
 
-class ConcreteErrorControllerTests: XCTestCase {
-    lazy var errorController = ConcreteErrorController()
-    
-    func testRequestBodyError() {
-        do {
-            try errorController.requestBodyError()
-            XCTFail("method should throw error")
-        } catch RequestError.InvalidBody {
-            XCTAssertTrue(true)
-        } catch {
-            XCTFail("wrong error type")
-        }
-    }
-    
-    func testRequestAuthenticationError() {
-        do {
-            try errorController.requestAuthenticationError()
-            XCTFail("method should throw error")
-        } catch RequestError.InvalidAuthentication {
-            XCTAssertTrue(true)
-        } catch {
-            XCTFail("wrong error type")
-        }
-    }
-    
-    func testRequestGeneralError() {
-        do {
-            try errorController.requestGeneralError()
-            XCTFail("method should throw error")
-        } catch RequestError.General {
-            XCTAssertTrue(true)
-        } catch {
-            XCTFail("wrong error type")
-        }
-    }
-    
-    func testRequestResponseDataEmpty() {
-        do {
-            try errorController.responseDataEmptyError()
-            XCTFail("method should throw error")
-        } catch ResponseError.InvalidResponseData {
-            XCTAssertTrue(true)
-        } catch {
-            XCTFail("wrong error type")
-        }
-    }
+class ConcreteErrorControllerSpec: QuickSpec {
 
-    func testRequestResponseError() {
-        let expectedError = NSError(domain: "com.icapps.test", code: 123, userInfo: [NSLocalizedDescriptionKey:"some error"])
-        XCTAssertThrowsError(try errorController.requestResponseError(expectedError), "method should trow correct error") { error in
-            guard let thrownError = error as? RequestError else {
-                XCTFail("wrong error type")
-                return
-            }
-            switch thrownError {
-            case .ResponseError(let responseError):
-                XCTAssertEqual(responseError, expectedError)
-            default:
-                XCTFail("wrong error type")
-            }
-        }
-    }
+	override func spec () {
+		describe("Concrete ErrorController default behaviour") {
+			let errorController = ConcreteErrorController()
+
+			it("should throw Request Errors for mistakes in the request") {
+				expect { try errorController.requestBodyError() }.to(throwError(closure: { (error) in
+					expect(error).to(matchError(RequestError.InvalidBody))
+				}))
+
+				expect { try errorController.requestAuthenticationError() }.to(throwError(closure: { (error) in
+					expect(error).to(matchError(RequestError.InvalidAuthentication))
+				}))
+
+				expect { try errorController.requestGeneralError()}.to(throwError(closure: { (error) in
+					expect(error).to(matchError(RequestError.General))
+				}))
+			}
+
+			it("should throw Response Errors when the response cannot be interpreted") {
+				expect { try errorController.responseDataEmptyError() }.to(throwError(closure: { (error) in
+					expect(error).to(matchError(ResponseError.InvalidResponseData))
+				}))
+
+				expect { try errorController.responseDataEmptyError() }.to(throwError(closure: { (error) in
+					let expectedNsError = NSError(domain: "101", code: 101, userInfo: nil)
+//					let responseError = ResponseError.ResponseError(error: expectedNsError)
+//					expect(error).to(matchError()
+				}))
+			}
+		}
+	}
+//
+//    func testRequestResponseError() {
+//        let expectedError = NSError(domain: "com.icapps.test", code: 123, userInfo: [NSLocalizedDescriptionKey:"some error"])
+//        XCTAssertThrowsError(try errorController.requestResponseError(expectedError), "method should trow correct error") { error in
+//            guard let thrownError = error as? RequestError else {
+//                XCTFail("wrong error type")
+//                return
+//            }
+//            switch thrownError {
+//            case .ResponseError(let responseError):
+//                XCTAssertEqual(responseError, expectedError)
+//            default:
+//                XCTFail("wrong error type")
+//            }
+//        }
+//    }
 }
