514dc1d9a10064cebf69b1c8e3ce85b0f411d671
Hannes Van den Berghe <hannes.van.den.berghe@live.be>
2016-04-28 10:05:14 +0200

    Added test with array in ResponseControllerSpec

--- Example/Tests/ResponseControllerSpec.swift
+++ Example/Tests/ResponseControllerSpec.swift
@@ -66,10 +66,22 @@ class ResponseControllerSpec: QuickSpec {
 						}, fail: { (error) in
 							XCTFail("Should not raise \(error)")
 					})
-
-
-					//TODO add array test
 				}
+                
+                it("Should succeed with invalid json if the mitigator handles the error with an array"){
+                    let expectedObjectId = "expectedObjectId"
+                    let expectedObjectId2 = "expectedObjectId2"
+                    let invalidDict = ["wrong": "json", "writeNode": [["objectId":expectedObjectId], ["objectId":expectedObjectId2]]]
+                    let data = try! NSJSONSerialization.dataWithJSONObject(invalidDict, options: .PrettyPrinted)
+                    
+                    ResponseController().respond(data, succeed: { (result: [MockEntityWithErrorMitigator]) in
+                        expect(result[0].objectId).to(equal(expectedObjectId))
+                        expect(result[1].objectId).to(equal(expectedObjectId2))
+                        expect(result.count).to(equal(2))
+                        }, fail: { (error) in
+                            print("Error occured \(error)")
+                    })
+                }
 			}
 		}
 	}
