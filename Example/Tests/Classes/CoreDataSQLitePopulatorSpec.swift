1acba11b0afaf4d3c4f3a720e1d400e6d31d711a
Hannes Van den Berghe <hannes.van.den.berghe@live.be>
2016-07-27 12:12:06 +0200

    #48 fixed

--- Example/Tests/Classes/CoreDataSQLitePopulatorSpec.swift
+++ Example/Tests/Classes/CoreDataSQLitePopulatorSpec.swift
@@ -22,6 +22,28 @@ class CoreDataSQLitePopulatorSpec: QuickSpec {
         }
     }
     
+    class MockPopulatorWithNO_versionPrefix_SQLite : CoreDataFromPopulatedSQLite {
+        override func allModelNameSQLiteFilesInDocumentsFolder() -> [NSURL]? {
+            return [NSURL.fileURLWithPath("file:bla/Test.sqlite")]
+        }
+    }
+    
+    class MockPopulatorNO_SQLiteFiles : CoreDataFromPopulatedSQLite {
+        override func allModelNameSQLiteFilesInDocumentsFolder() -> [NSURL]? {
+            return nil
+        }
+    }
+    
+    class MockPopulatorWithMultipleSQLiteFiles : CoreDataFromPopulatedSQLite {
+        override func allModelNameSQLiteFilesInDocumentsFolder() -> [NSURL]? {
+            let u1 = NSURL.fileURLWithPath("file:bla/1_Test.sqlite")
+            let u2 = NSURL.fileURLWithPath("file:bla/2_Test.sqlite")
+            let u3 = NSURL.fileURLWithPath("file:bla/3_Test.sqlite")
+            
+            return [u1, u2, u3]
+        }
+    }
+    
     override func spec() {
         describe("CoreDataSQLitePopulatorSpec") {
             
@@ -31,7 +53,31 @@ class CoreDataSQLitePopulatorSpec: QuickSpec {
                 expect(populator.reuseSQLite()).to(beTrue())
             })
             
-            //Add test for no prefix
+            
+            it("should NOT reuse sqlite with different version", closure: {
+                let populator = MockPopulatorWithSQLite(modelName: "Test", version: "2")
+                
+                expect(populator.reuseSQLite()).to(beFalse())
+            })
+            
+            it("should NOT reuse sqlite when multiple files available", closure: {
+                let populator = MockPopulatorWithMultipleSQLiteFiles(modelName: "Test", version: "1")
+                
+                expect(populator.reuseSQLite()).to(beFalse())
+            })
+            
+            it("should NOT reuse sqlite when no files available", closure: {
+                let populator = MockPopulatorNO_SQLiteFiles(modelName: "Test", version: "1")
+                
+                expect(populator.reuseSQLite()).to(beFalse())
+            })
+            
+            
+            it("should NOT reuse sqlite when no files available", closure: {
+                let populator = MockPopulatorWithNO_versionPrefix_SQLite(modelName: "Test", version: "1")
+                
+                expect(populator.reuseSQLite()).to(beFalse())
+            })
             
         }
     }
