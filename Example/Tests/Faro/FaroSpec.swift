3e6fe3e96f7bb7625f08b8675fd13db648a41c13
StijnWillems <stijn.willems@icapps.com>
2016-08-31 13:24:09 +0200

    First test!!!!

--- Example/Tests/Faro/FaroSpec.swift
+++ Example/Tests/Faro/FaroSpec.swift
@@ -1,34 +1,19 @@
-//
-//  FaroSpec.swift
-//  Faro
-//
-//  Created by Stijn Willems on 31/08/16.
-//  Copyright © 2016 CocoaPods. All rights reserved.
-//
+import Quick
+import Nimble
 
-import XCTest
+import Faro
+@testable import Faro_Example
 
-class FaroSpec: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class FaroSpec: QuickSpec {
+
+    override func spec() {
+        describe("Faro") {
+
+            context("success") {
+                it("should succeed"){
+
+                }
+            }
         }
     }
     
