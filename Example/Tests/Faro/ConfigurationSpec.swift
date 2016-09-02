2fc74001361bba026c69b519ef51cb3d89e51abc
StijnWillems <stijn.willems@icapps.com>
2016-09-02 16:45:55 +0200

    Configuration has an url

--- Example/Tests/Faro/ConfigurationSpec.swift
+++ Example/Tests/Faro/ConfigurationSpec.swift
@@ -6,13 +6,17 @@ import Faro
 class ConfigurationSpec: QuickSpec {
     override func spec() {
         describe("Configuration") {
+            let expected = "http://www.something.be"
+            let configuration = Configuration(baseURL: expected)
 
             context("initialisation") {
                 it("should have a baseURL"){
-                    let expected = "http://www.something.be"
-                    let configuration = Configuration(baseURL: expected)
-
                     expect(configuration.baseURL).to(equal(expected))
+                }
+
+                it("should have a valid NSURL") {
+                    expect(configuration.url).toNot(beNil())
+                    expect(configuration.url?.absoluteString).to(equal(expected))
                 }
             }
         }
--- Sources/Configuration.swift
+++ Sources/Configuration.swift
@@ -11,6 +11,11 @@ import Foundation
 /// Use for different configurations for the specific environment you want to use for *Bar.*
 public class Configuration {
     public let baseURL : String
+    public var url : NSURL? {
+        get {
+            return NSURL(string: baseURL)
+        }
+    }
 
     public init(baseURL: String) {
         self.baseURL = baseURL
