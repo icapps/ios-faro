96766f44a9370abda15159933802e2b717dd1433
StijnWillems <stijn.willems@icapps.com>
2016-09-02 16:45:30 +0200

    Order has a method

--- Sources/Order.swift
+++ Sources/Order.swift
@@ -1,8 +1,12 @@
 
-public class Order {
-    public let path: String
+public enum Method: String { case GET, POST, PUT, DELETE, PATCH }
 
-    public init (path: String) {
+public class Order {
+    public let path : String
+    public let method : Method
+
+    public init (path: String, method: Method = .GET) {
         self.path = path
+        self.method = method
     }    
 }
