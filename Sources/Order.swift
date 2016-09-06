37967db52b6b2d17bde93005688aee0bfc6e5035
StijnWillems <stijn.willems@icapps.com>
2016-09-06 16:12:23 +0200

    Added todo’s and whishlist for an order. Still to be tested

--- Sources/Order.swift
+++ Sources/Order.swift
@@ -2,17 +2,51 @@ public enum Method: String {
     case GET, POST, PUT, DELETE, PATCH
 }
 
+// TODO: Add tests for these rules
+
+/// The `EngagementRule` enum defines how deep a JSON is to be parsed
+/**
+ ```
+    { node:subnode:{node:subnode:...}
+ ```
+*/
+public enum EngagementRule {
+    case None
+    case ObjectDetermined // The rules of the Type to be parsed should be used.
+    case Inline // Only the data that you already have
+    case Exhaustive // fetch all sub entities via new service requests
+    case ExhaustiveRecursive // fetch all sub entities of sub entities (nuclear)
+}
+
 public class Order {
+    public typealias Rules = [(key: String, rule: EngagementRule)]
     public let path: String
     public let method: Method
 
-    public init(path: String, method: Method = .GET) {
+    /// The rules for the nodes that should be fetched.
+    public let rulesOfEngagement: Rules?
+
+    public init(path: String, method: Method = .GET, rulesOfEngagement: Rules? = nil) {
         self.path = path
         self.method = method
+        self.rulesOfEngagement = rulesOfEngagement
     }
 
-    public func urlForConfiguration(configuration: Configuration) -> NSURL? {
-        return NSURL(string: "\(configuration.baseURL)/\(path)")
+    public func objectRequestConfiguration(configuration: Configuration) -> NSURLRequest? {
+
+        // TODO: add GET
+        return NSURLRequest(URL: NSURL(string: "\(configuration.baseURL)/\(path)")!)
     }
 
+    public func collectionRequestForConfiguration(configuration: Configuration) -> NSURLRequest? {
+        return nil
+    }
+
+    /// parse sub nodes or not
+    func shouldParseNodeWithKey(key: String) -> Bool {
+        // TODO: test rules
+        return true
+    }
+
+    // TODO: Add ability to paginate
 }
