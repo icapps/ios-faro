21c82d3ffef79dd12f7f3b1d1b49e9245aa53cb6
StijnWillems <stijn.willems@icapps.com>
2016-09-06 10:30:52 +0200

    Coding guidelines

--- Example/Example/Model/Model.swift
+++ Example/Example/Model/Model.swift
@@ -1,17 +1,18 @@
-
 import Faro
 
+// TODO: we should add a more generic parser
+
 /// Example model
-/// TODO: we should add a more generic parser
 /// We inherit from NSObject to be useable in Objective-C
-class Model : NSObject, Mappable {
-    var value : String
+class Model: NSObject, Mappable {
+    var value: String
 
     required init(json: AnyObject) {
-        if let json = json as? [String : String] {
+        if let json = json as? [String: String] {
             value = json["key"]!
         }else {
             value = ""
         }
     }
+
 }
--- Example/Tests/Faro/OrderSpec.swift
+++ Example/Tests/Faro/OrderSpec.swift
@@ -5,13 +5,14 @@ import Faro
 @testable import Faro_Example
 
 class OrderSpec: QuickSpec {
+    
     override func spec() {
         describe("Order") {
             let expected = "path"
             let order = Order(path: expected)
 
             context("initialisation") {
-                it("should have a path"){
+                it("should have a path") {
                     expect(order.path).to(equal(expected))
                 }
 
@@ -21,4 +22,5 @@ class OrderSpec: QuickSpec {
             }
         }
     }
+
 }
