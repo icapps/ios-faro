eb1bfe2f85d3b74053551717b0f3a3df4e99bd55
StijnWillems <stijn.willems@icapps.com>
2016-09-08 12:22:45 +0200

    As discussed with Kris added EngagementRule placeholder to Order

--- Example/Tests/Faro/OrderSpec.swift
+++ Example/Tests/Faro/OrderSpec.swift
@@ -7,27 +7,37 @@ import Faro
 class OrderSpec: QuickSpec {
 
     override func spec() {
-        describe("Order") {
+        describe("Order .GET") {
             let expected = "path"
             let order = Order(path: expected)
             let configuration = Faro.Configuration(baseURL: "http://someURL")
 
-            context("initialisation . GET") {
+            it("should have a path") {
+                expect(order.path).to(equal(expected))
+            }
 
-                it("should have a path") {
-                    expect(order.path).to(equal(expected))
-                }
+            it("should default to .GET") {
+                let request = order.objectRequestConfiguration(configuration)!
+                expect(request.HTTPMethod).to(equal("GET"))
+            }
 
-                it("should default to .GET") {
-                    let request = order.objectRequestConfiguration(configuration)!
-                    expect(request.HTTPMethod).to(equal("GET"))
-                }
+            it("should configuration should make up request") {
+                let request = order.objectRequestConfiguration(configuration)!
+                expect(request.URL!.absoluteString).to(equal("http://someURL/path"))
+            }
 
-                it("should configuration should make up request") {
+        }
 
-                    let request = order.objectRequestConfiguration(configuration)!
-                    expect(request.URL!.absoluteString).to(equal("http://someURL/path"))
-                }
+        describe("Order Engagementrule") {
+            it("should have rule .None or All for a nodeKey") {
+                let rules = [(nodeKey: "key 1", rule: EngagementRule.None), (nodeKey: "key 2", rule: EngagementRule.All)]
+                let order = Order(path: "", rulesOfEngagement: rules)
+
+                let rule1 = order.engagementRuleForNodeKey("key 1")
+                let rule2 = order.engagementRuleForNodeKey("key 2")
+
+                expect(rule1 == .None).to(beTrue())
+                expect(rule2 == .All).to(beTrue())
             }
         }
     }
--- Sources/Order.swift
+++ Sources/Order.swift
@@ -2,24 +2,8 @@ public enum Method: String {
     case GET, POST, PUT, DELETE, PATCH
 }
 
-// TODO: Add tests for these rules
-
-/// The `EngagementRule` enum defines how deep a JSON is to be parsed
-/**
- ```
-    { node:subnode:{node:subnode:...}
- ```
-*/
-public enum EngagementRule {
-    case None
-    case ObjectDetermined // The rules of the Type to be parsed should be used.
-    case Inline // Only the data that you already have
-    case Exhaustive // fetch all sub entities via new service requests
-    case ExhaustiveRecursive // fetch all sub entities of sub entities (nuclear)
-}
-
 public class Order {
-    public typealias Rules = [(key: String, rule: EngagementRule)]
+    public typealias Rules = [(nodeKey: String, rule: EngagementRule)]
     public let path: String
     public let method: Method
 
@@ -43,10 +27,17 @@ public class Order {
         return nil
     }
 
-    /// parse sub nodes or not
-    func shouldParseNodeWithKey(key: String) -> Bool {
-        // TODO: test rules
-        return true
+    /// Parse sub nodes or not
+    public func engagementRuleForNodeKey(nodeKey: String) -> EngagementRule {
+        guard let rulesOfEngagement = rulesOfEngagement else {
+            return .None
+        }
+
+        if let rule = (rulesOfEngagement.filter { $0.nodeKey == nodeKey }).first {
+            return rule.rule
+        } else {
+            return .None
+        }
     }
 
     // TODO: Add ability to paginate
