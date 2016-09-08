c713e38c06f336f5c1e229dcec687ea205c1697a
StijnWillems <stijn.willems@icapps.com>
2016-09-08 17:32:11 +0200

    check statusCodeAndDat is a function that can print.
    The thrower thing gave memory issues so it was removed and some extra error options are added.

--- Sources/ResultEnum.swift
+++ Sources/ResultEnum.swift
@@ -3,30 +3,9 @@ public enum Result<M: Mappable> {
     case Model(M)
     /// The server returned a valid JSON response.
     case JSON(AnyObject)
-    case Failure(ErrorType)
+    case Data(NSData)
+    /// Server returned with statuscode 200...201 but no response data
+    case OK
+    case Failure(Error)
 }
 
-/// Catches any throws and switches if to af failure after printing the error.
-public func convertAllThrowsToResult<M: Mappable>(result: (Result<M>) -> (), thrower: () throws -> ()) {
-    do {
-        try thrower()
-    } catch Error.Error(let nserror) {
-        print("💣 Error from service: \(nserror)")
-        result(.Failure(Error.Error(nserror)))
-    } catch Error.General {
-        print("💣 General service error")
-        result(.Failure(Error.General))
-    } catch Error.InvalidResponseData(let data) {
-        print("🤔 Invalid response data")
-        result(.Failure(Error.InvalidResponseData(data)))
-    } catch Error.InvalidAuthentication {
-        print("💣 Invalid authentication")
-        result(.Failure(Error.InvalidAuthentication))
-    } catch Error.ShouldOverride {
-        print("💣 You should override this method")
-        result(.Failure(Error.ShouldOverride))
-    } catch {
-        print("💣 failed with unknown error \(error)")
-        result(.Failure(error))
-    }
-}
--- Sources/Service.swift
+++ Sources/Service.swift
@@ -5,34 +5,84 @@ public class Service {
         self.configuration = configuration
     }
 
-    /// You should override this and could use it like in `JSONService`
+    /// You should override this. Example use can be found in `JSONService`
     public func serve<M: Mappable>(order: Order, result: (Result <M>) -> ()) {
         result(.Failure(Error.ShouldOverride))
     }
 
-    public func checkStatusCodeAndData(data: NSData?, urlResponse: NSURLResponse?, error: NSError?) throws -> NSData? {
-        guard error == nil else {
-            throw Error.Error(error)
-        }
-
-        guard let httpResponse = urlResponse as? NSHTTPURLResponse else {
-            return data
-        }
-
-        let statusCode = httpResponse.statusCode
-        guard statusCode != 404 else {
-            throw Error.InvalidAuthentication
-        }
-
-        guard 200...201 ~= statusCode else {
-            return data
-        }
-
-        guard let guardData = data else {
-            return nil
-        }
-        return guardData
-    }
-
 }
 
+public func checkStatusCodeAndData<M: Mappable>(data: NSData?, urlResponse: NSURLResponse?, error: NSError?, result: (Result<M>) -> ()) {
+    guard error == nil else {
+        let returnError = Error.ErrorNS(error)
+        printError(returnError)
+        result(.Failure(returnError))
+        return
+    }
+
+    guard let httpResponse = urlResponse as? NSHTTPURLResponse else {
+        let returnError = Error.General
+        printError(returnError)
+        result(.Failure(returnError))
+        return
+    }
+
+    let statusCode = httpResponse.statusCode
+    guard statusCode != 404 else {
+        let returnError = Error.InvalidAuthentication
+        printError(returnError)
+        result(.Failure(returnError))
+        return
+    }
+
+    guard 200...201 ~= statusCode else {
+        let returnError = Error.General
+        printError(returnError)
+        result(.Failure(returnError))
+        return
+    }
+
+    guard let guardedData = data else {
+        result(.OK)
+        return
+    }
+
+    result(.Data(guardedData))
+
+    return
+}
+
+public func serializeJSONFromDataResult<M: Mappable>(dataResult: Result<M>, jsonResult: (Result <M>) -> ()) {
+    switch dataResult {
+    case .Data(let data):
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            jsonResult(.JSON(json))
+        } catch {
+            jsonResult(.Failure(Error.Error(error)))
+        }
+    default:
+        jsonResult(dataResult)
+    }
+}
+
+/// Catches any throws and switches if to af failure after printing the error.
+public func printError(error: Error) {
+    switch error {
+    case Error.Error(let error):
+        print("💣 Error from service: \(error)")
+    case Error.ErrorNS(let nserror):
+        print("💣 Error from service: \(nserror)")
+    case Error.General:
+        print("💣 General service error")
+    case Error.InvalidResponseData(_):
+        print("🤔 Invalid response data")
+    case Error.InvalidAuthentication:
+        print("💣 Invalid authentication")
+    case Error.ShouldOverride:
+        print("💣 You should override this method")
+    default:
+        print("💣 failed with unknown error \(error)")
+
+    }
+}
