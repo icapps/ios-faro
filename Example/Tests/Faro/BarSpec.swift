dc0b3bbd68553fea8ec405cc3ed38a70ac3885e1
StijnWillems <stijn.willems@icapps.com>
2016-09-08 17:37:38 +0200

    refactor barSpec to have a dedicated service

--- Example/Tests/Faro/BarSpec.swift
+++ Example/Tests/Faro/BarSpec.swift
@@ -8,29 +8,25 @@ import Faro
 class BarSpec: QuickSpec {
     override func spec() {
         describe("Bar") {
-            context("when Bar returns with Result.Model()") {
-                var baseURL: String!
-                var configuration: Faro.Configuration!
+            context(".Model(model)") {
                 var bar: Bar!
                 var mockJSON: AnyObject!
 
                 beforeEach({
                     mockJSON = ["key": "value"]
-                    baseURL = "http://www.something.be"
-                    configuration = Configuration(baseURL: baseURL)
-                    bar = Bar(configuration: configuration)
+                    bar = Bar(service: MockService(mockJSON: mockJSON))
 
                 })
 
                 it("should have a configuration with the correct baseUrl") {
-                    expect(bar.configuration.baseURL).to(equal(baseURL))
+                    expect(bar.service.configuration.baseURL).to(equal("mockService"))
                 }
 
                 it("should return in sync with the mock model") {
                     let order = Order(path: "unitTest")
                     var isInSync = false
 
-                    bar.serve(order, service: MockService(mockJSON: mockJSON)) { (result: Result <MockModel>) in
+                    bar.serve(order) { (result: Result <MockModel>) in
                         isInSync = true
                         switch result {
                         case .Model(model: let model):
