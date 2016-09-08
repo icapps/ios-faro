95939dab9c55708d2bf4d31fcd4ecdcabf3ca269
StijnWillems <stijn.willems@icapps.com>
2016-09-08 17:37:59 +0200

    error checking improved

--- Example/Tests/Faro/ServiceSpec.swift
+++ Example/Tests/Faro/ServiceSpec.swift
@@ -4,24 +4,6 @@ import Nimble
 import Faro
 @testable import Faro_Example
 
-func checkToBeError(expectedError: Error?, service: Service, data: NSData? = nil, response: NSURLResponse? = nil, nsError: NSError? = nil) -> Bool {
-
-    var error: Error?
-    let result = { (result: Result <MockModel>) in
-        switch result {
-        case .Failure(let returnError):
-            error = returnError as? Error
-        default:
-            return
-        }
-    }
-    convertAllThrowsToResult(result) {
-        try service.checkStatusCodeAndData(data, urlResponse: response, error: nsError)
-    }
-
-    return error == expectedError
-}
-
 class ServiceSpec: QuickSpec {
     override func spec() {
         describe("MockService") {
@@ -50,64 +32,122 @@ class ServiceSpec: QuickSpec {
 
             it("InvalidAuthentication when statuscode 404") {
                 let response = NSHTTPURLResponse(URL: NSURL(), statusCode: 404, HTTPVersion: nil, headerFields: nil)
-                expect(checkToBeError(Error.InvalidAuthentication, service: service, response: response)).to(beTrue())
+                checkStatusCodeAndData(nil, urlResponse: response, error: nil) { (result: Result<MockModel>) in
+                    switch result {
+                    case .Failure(let faroError) where faroError == Error.InvalidAuthentication:
+                        break
+                    default:
+                        XCTFail("Should have invalid authentication error")
+                    }
+                }
             }
 
             it("Fail for NSError") {
-                let expected = NSError(domain: "tests", code: 101, userInfo: nil)
-                expect(checkToBeError(Error.Error(expected), service: service, nsError: expected)).to(beTrue())
+                let nsError = NSError(domain: "tests", code: 101, userInfo: nil)
+                checkStatusCodeAndData(nil, urlResponse: nil, error: nsError) { (result: Result<MockModel>) in
+                    switch result {
+                    case .Failure(let faroError):
+                        switch faroError {
+                        case .ErrorNS(_):
+                            break
+                        default:
+                            XCTFail("Should have invalid authentication error")
+
+                        }
+                    default:
+                        XCTFail("Should have invalid authentication error")
+                    }
+                }
             }
 
-            it("No fail for statuscode 200") {
-                let response = NSHTTPURLResponse(URL: NSURL(), statusCode: 200, HTTPVersion: nil, headerFields: nil)
-                expect(checkToBeError(nil, service: service, response: response)).to(beTrue())
+            context("no data from service") {
+                it("No fail for statuscode 200") {
+                    ExpectResponse.statusCode(200)
+                }
+
+                it("No fail for statuscode 201") {
+                    ExpectResponse.statusCode(201)
+                }
+            }
+
+            context("data from service") {
+                it("data returned for statuscode 200") {
+                    ExpectResponse.statusCode(200, data: "data".dataUsingEncoding(NSUTF8StringEncoding))
+                }
+
+                it("data returned for statuscode 201") {
+                    ExpectResponse.statusCode(201, data: "data".dataUsingEncoding(NSUTF8StringEncoding))
+                }
+            }
+
+            describe("JSONService Asynchronous", {
+                it("should fail for a wierd url") {
+                    let configuration = Faro.Configuration(baseURL: "wierd")
+                    let service = JSONService(configuration: configuration)
+                    let order = Order(path: "posts")
+
+                    var failed = false
+
+                    service.serve(order) { (result: Result <MockModel>) in
+                        switch result {
+                        case .Failure:
+                            failed = true
+                        default:
+                            XCTFail("💣should fail")
+                        }
+                    }
+
+                    expect(failed).toEventually(beTrue())
+                }
+
+                it("should return an empty model") {
+                    let configuration = Faro.Configuration(baseURL: "http://jsonplaceholder.typicode.com")
+                    let service = JSONService(configuration: configuration)
+                    let order = Order(path: "posts")
+
+                    var receivedJSON = false
+
+                    service.serve(order) { (result: Result <MockModel>) in
+                        switch result {
+                        case .JSON(let json):
+                            if let json = json as? [[String: AnyObject]] {
+                                expect(json.count).to(equal(100))
+                                receivedJSON = true
+                            } else {
+                                XCTFail("\(json) is wrong")
+                            }
+                        default:
+                            XCTFail("💣should return json")
+                        }
+                    }
+
+                    expect(receivedJSON).toEventually(beTrue())
+                }
+            })
+        }
+    }
+}
+
+class ExpectResponse {
+    static func statusCode(statusCode: Int, data: NSData? = nil) {
+        let response = NSHTTPURLResponse(URL: NSURL(), statusCode: statusCode, HTTPVersion: nil, headerFields: nil)
+        checkStatusCodeAndData(data, urlResponse: response, error: nil) { (result: Result<MockModel>) in
+            if let data = data {
+                switch result {
+                case .Data(_):
+                    break
+                default:
+                    XCTFail("Should not fail for statuscode: \(statusCode) data: \(data)")
+                }
+            } else {
+                switch result {
+                case .OK:
+                    break
+                default:
+                    XCTFail("Should not fail for statuscode: \(statusCode) data: \(data)")
+                }
             }
 
         }
-
-        describe("JSONService Asynchronous", {
-            it("should fail for a wierd url") {
-                let configuration = Faro.Configuration(baseURL: "wierd")
-                let service = JSONService(configuration: configuration)
-                let order = Order(path: "posts")
-
-                var failed = false
-
-                service.serve(order) { (result: Result <MockModel>) in
-                    switch result {
-                    case .Failure:
-                        failed = true
-                    default:
-                        XCTFail("💣should fail")
-                    }
-                }
-
-                expect(failed).toEventually(beTrue())
-            }
-
-            it("should return an empty model") {
-                let configuration = Faro.Configuration(baseURL: "http://jsonplaceholder.typicode.com")
-                let service = JSONService(configuration: configuration)
-                let order = Order(path: "posts")
-
-                var receivedJSON = false
-
-                service.serve(order) { (result: Result <MockModel>) in
-                    switch result {
-                    case .JSON(let json):
-                        if let json = json as? [[String: AnyObject]] {
-                            expect(json.count).to(equal(100))
-                            receivedJSON = true
-                        } else {
-                            XCTFail("\(json) is wrong")
-                        }
-                    default:
-                        XCTFail("💣should return json")
-                    }
-                }
-
-                expect(receivedJSON).toEventually(beTrue())
-            }
-        })
     }
 }
