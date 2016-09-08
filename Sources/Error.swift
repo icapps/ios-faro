a6466055b1a686dc6abbb33dfa6456d4d4b8bee2
StijnWillems <stijn.willems@icapps.com>
2016-09-08 17:35:36 +0200

    added ns error case and ErroType case

--- Sources/Error.swift
+++ Sources/Error.swift
@@ -2,7 +2,8 @@ public enum Error: ErrorType, Equatable {
     case General
     case InvalidUrl(String)
     case InvalidResponseData(NSData?)
-    case Error(NSError?)
+    case ErrorNS(NSError?)
+    case Error(ErrorType)
     case InvalidAuthentication
     case ShouldOverride
     case CreateDataTask
@@ -12,13 +13,15 @@ public func == (lhs: Error, rhs: Error) -> Bool {
     switch (lhs, rhs) {
     case (.General, .General):
         return true
-    case (.InvalidAuthentication, .InvalidAuthentication ):
+    case (.InvalidAuthentication, .InvalidAuthentication):
         return true
     case (.InvalidUrl(let url_lhs), .InvalidUrl(let url_rhs)): // tailor:disable
         return url_lhs == url_rhs
-    case (.Error(let error_lhs), .Error(let error_rhs)): // tailor:disable
+    case (.Error(_), .Error(_)):
+        return true
+    case (.ErrorNS(let error_lhs), .ErrorNS(let error_rhs)): // tailor:disable
         return error_lhs?.code == error_rhs?.code
-    case (.InvalidResponseData (_), .InvalidResponseData ( _)):
+    case (.InvalidResponseData (_), .InvalidResponseData (_)):
         return true
     default:
         return false
