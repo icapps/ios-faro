ea7b5c0ee730a36dc785419ee56347b933a67795
StijnWillems <stijn.willems@icapps.com>
2016-09-16 13:32:50 +0200

    minor

--- Example/Example/Modules/Swift/SwiftViewController.swift
+++ Example/Example/Modules/Swift/SwiftViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import Faro
 import Stella
 
-class Posts: Parseable {
+class Post: Parseable {
 
     required init?(from raw: Any) {
         
@@ -23,7 +23,7 @@ class SwiftViewController: UIViewController {
         let service = ExampleService()
         let call = Call(path: "posts")
 
-        service.perform(call) { (result: Result<Posts>) in
+        service.perform(call) { (result: Result<Post>) in
             DispatchQueue.main.async {
                 switch result {
                 case .model(let model):
