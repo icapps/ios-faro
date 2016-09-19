47c931b923c14d369b3365235f64042795b0f520
StijnWillems <stijn.willems@icapps.com>
2016-09-19 13:21:56 +0200

    rename extractRootNode to RootNode and moved to Call

--- Example/Example/Model/Model.swift
+++ Example/Example/Model/Model.swift
@@ -14,15 +14,5 @@ class Model: Parseable {
     var JSON: [String: Any]? {
         return nil
     }
-
-    class func extractRootNode(from json: Any) -> JsonNode {
-        if let jsonArray = json as? [[String: Any]] {
-            return .rootNodes(jsonArray)
-        }else if let json = json as? [String: Any] {
-            return .rootNode(json)
-        }else {
-            return .rootNodeNotFound(json: json)
-        }
-    }
     
 }
--- Example/Tests/TestHelpers/MockModel.swift
+++ Example/Tests/TestHelpers/MockModel.swift
@@ -18,15 +18,4 @@ class MockModel: Parseable {
     var JSON: [String: Any]? {
         return nil
     }
-
-    class func extractRootNode(from json: Any) -> JsonNode {
-        if let jsonArray = json as? [Any] {
-            return .rootNodes(jsonArray)
-        }else if let json = json as? [String: Any] {
-            return .rootNode(json)
-        }else {
-            return .rootNodeNotFound(json: json)
-        }
-    }
-
 }
