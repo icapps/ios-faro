9752203f79ff3486445da99ad39b3d510c03e4a3
Ben Algoet <ben.algoet@icapps.com>
2016-09-21 14:56:29 +0200

    FARO-26 Failed test with deserialize operator <-> to object

--- Example/Tests/DeserializeOperatorsSpec.swift
+++ Example/Tests/DeserializeOperatorsSpec.swift
@@ -4,8 +4,6 @@ import Nimble
 import Faro
 @testable import Faro_Example
 
-
-
 class DeserializeOperatorsSpec: QuickSpec {
 
     override func spec() {
@@ -23,6 +21,18 @@ class DeserializeOperatorsSpec: QuickSpec {
                     zoo <-> json
 
                     expect(zoo?.animalArray?.count) == 2
+                }
+                
+                it("should deserialize to object") {
+                    let randomNumber = "randomNumber"
+                    let json = ["cellNumber": randomNumber, "foodTicket": "ticket"] as Any?
+                    
+                    var gail = Gail(from: ["":""])
+                    
+                    gail <-> json
+                    
+                    expect(gail?.cellNumber) == "Hello"
+                    
                 }
 
             }
