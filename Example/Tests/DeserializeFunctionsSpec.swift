82b4d9940b79d5577c8deaffcba1a3ab886f11f1
Ben Algoet <ben.algoet@icapps.com>
2016-09-22 13:22:01 +0200

    FARO-26 successful test with parsing generic value from JSON

--- Example/Tests/DeserializeFunctionsSpec.swift
+++ Example/Tests/DeserializeFunctionsSpec.swift
@@ -9,9 +9,19 @@ class DeserializeFunctionSpec: QuickSpec {
     override func spec() {
         describe("DeserializeFunctionSpec") {
 
-            it("should have test") {
-                //TODO: FARO-26
-//                XCTFail("Please add tests")
+            it("should parse generic value from JSON") {
+                let uuidKey = "uuid"
+                let json = [uuidKey:"some id"] as [String : Any]
+                let o1 = DeserializableObject(from: ["":""])
+                
+                do {
+                    o1?.uuid = try parse(uuidKey, from: json)
+                } catch {
+                    return
+                }
+                
+                expect(o1?.uuid) == json["uuid"] as? String
+            }
             }
         }
     }
