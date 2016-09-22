fe2fee26128cd4ffebd0f66cd7143f66055b3ae7
StijnWillems <stijn.willems@icapps.com>
2016-09-22 16:01:08 +0200

    refactor

--- Example/Tests/DeserializeFunctionsSpec.swift
+++ Example/Tests/DeserializeFunctionsSpec.swift
@@ -11,75 +11,54 @@ class DeserializeFunctionSpec: QuickSpec {
 
             it("should parse generic value from JSON") {
                 let uuidKey = "uuid"
-                let json = [uuidKey:"some id"] as [String : Any]
-                let o1 = DeserializableObject(from: ["":""])
+                let json = [uuidKey:"some id" as Any]
+                let o1 = DeserializableObject(from: ["":""])!
                 
-                do {
-                    o1?.uuid = try parse(uuidKey, from: json)
-                } catch {
-                    return
-                }
-                
-                expect(o1?.uuid) == json["uuid"] as? String
+                o1.uuid = try! parse(uuidKey, from: json)
+
+                expect(o1.uuid) == json["uuid"] as? String
             }
             
             it("should parse Date from JSON with TimeInterval") {
                 let dateKey = "date"
                 let dateTimeInterval: TimeInterval = 12345.0
                 let json = ["date": dateTimeInterval]
-                let o1 = DeserializableObject(from: ["":""])
+                let o1 = DeserializableObject(from: ["":""])!
                 
-                do {
-                    o1?.date = try parse(dateKey, from: json)
-                } catch {
-                    return
-                }
-                
+                o1.date = try! parse(dateKey, from: json)
+
                 let date = Date(timeIntervalSince1970: json["date"]!)
-                expect(o1?.date) == date
+                expect(o1.date) == date
             }
             
             it("should parse Date from JSON with String") {
                 let dateKey = "date"
                 let dateString = "1994-08-20"
-                let json = ["date": dateString]
-                let o1 = DeserializableObject(from: ["":""])
+                let json = [dateKey: dateString as Any]
+                let o1 = DeserializableObject(from: ["":""])!
                 
-                do {
-                    setDateFormat("yyyy-MM-dd")
-                    o1?.date = try parse(dateKey, from: json)
-                } catch {
-                    return
-                }
-                
-                expect(o1?.date).toNot(beNil())
+                o1.date = try! parse(dateKey, from: json, format: "yyyy-MM-dd")
+
+                expect(o1.date).toNot(beNil())
             }
             
             it("should parse generic object from JSON") {
-                let json = ["uuid":"some id"] as [String: Any]
-                var o1 = DeserializableObject(from: ["":""])
+                let json = ["uuid":"some id" as Any]
+                var o1 = DeserializableObject(from: ["":""])!
                 
-                do {
-                    o1 = try parse(from: json)
-                } catch {
-                    return
-                }
-                
-                expect(o1?.uuid) == json["uuid"] as! String?
+                o1 = try! parse(from: json)!
+
+                expect(o1.uuid) == json["uuid"] as! String?
             }
             
             it("should parse generic object arrays from JSON") {
                 let json = [["uuid": "id1"],["uuid":"id2"]]
-                let o1 = DeserializableObject(from: ["":""])
-                let o2 = DeserializableObject(from: ["":""])
-                var objectArray = [o1!, o2!]
+                let o1 = DeserializableObject(from: ["":""])!
+                let o2 = DeserializableObject(from: ["":""])!
+                var objectArray = [o1, o2]
                 
-                do {
-                    objectArray = try parse(from: json)!
-                } catch {
-                    return
-                }
-            
+                objectArray = try! parse(from: json)!
+
                 expect(objectArray.count) == 2
                 expect(objectArray.first?.uuid) == json.first?["uuid"]
                 expect(objectArray.last?.uuid) == json.last?["uuid"]
