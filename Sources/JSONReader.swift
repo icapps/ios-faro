5dfbacbb1cff64552dfd47ea5ef9b3cae0eb68aa
Ben Algoet <ben.algoet@icapps.com>
2016-09-27 15:14:09 +0200

    FARO-27 Create JSONReader in Faro to read data from assets

---
+++ Sources/JSONReader.swift
@@ -0,0 +1,24 @@
+//
+//  JSONReader.swift
+//  Pods
+//
+//  Created by Ben Algoet on 27/09/16.
+//
+//
+
+import UIKit
+
+class JSONReader: NSObject {
+    static func parseFile(named: String!) -> [String : Any]? {
+        do {
+            guard let data = NSDataAsset(name: named, bundle: Bundle.init(for: self))?.data else {
+                return nil
+            }
+            
+            return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
