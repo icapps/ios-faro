a8ffecc6ba1370a515f01f09ca436f26374cc387
Ben Algoet <ben.algoet@icapps.com>
2016-09-27 15:17:09 +0200

    FARO-27 Create applicationKeys in test for ServiceTest

---
+++ Example/Tests/TestHelpers/ApplicationKeys.swift
@@ -0,0 +1,21 @@
+//
+//  ApplicationKeys.swift
+//  Faro
+//
+//  Created by Ben Algoet on 27/09/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import Stella
+
+struct ApplicationKeys {
+    
+    static let sharedInstance = ApplicationKeys()
+    
+    var shouldMockServices: Bool {
+        printBreadcrumb("Mocking all services")
+        return true
+    }
+
+}
