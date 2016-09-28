58c65f0f9d7eb5c228aa375000d41c78f1e6ff3f
StijnWillems <stijn.willems@icapps.com>
2016-09-28 09:58:51 +0200

    FARO-27 mockService integrated with old mockService

--- Sources/FaroService.swift
+++ Sources/FaroService.swift
@@ -1,30 +1,33 @@
-//
-//  FaroService
-//  Pods
-//
-//  Created by Ben Algoet on 27/09/16.
-//
-//
-
 import UIKit
 
+/// Uses as a global to determine if `FaroService` should do reald data
 public struct MockSwitch {
     public static var shouldMock = false
 }
 
+/// You can use this as a singleton that switches betwee using real data or data from a file.
+/// To switch we use `MockSwitch`
 open class FaroService: Service {
-    
+    /// Sends its requests to the servers
     private static var sharedService: Service?
-    
-    public init(with baseURL: String) {
+    /// Sends its requests to a file
+    private static let sharedMockService = MockService()
+
+
+    public static func setup(with baseURL: String) {
         FaroService.sharedService = Service(configuration: Configuration(baseURL: baseURL))
-        super.init(configuration: Configuration(baseURL: baseURL))
     }
     
-    private static let sharedMockService = MockService()
-    
+    /// Returns real or mocked service. Run `setup(with:)` before calling this function.
+    /// If you don't we default to returning the MockService
     public static var shared: Service {
-        return MockSwitch.shouldMock ? sharedMockService : sharedService!
+        if let sharedService = FaroService.sharedService {
+            return MockSwitch.shouldMock ? sharedMockService : sharedService
+        } else {
+            print("❓ You should run FaroService.setup(with:) in the AppDelegate after startup to have a service pointing to a service.")
+            print("🍞 Fallin back on using the MockService.")
+            return FaroService.sharedMockService
+        }
     }
     
 }
