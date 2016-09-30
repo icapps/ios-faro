93db29e74c7cdd096bf72ba6e1075557c7210501
StijnWillems <stijn.willems@icapps.com>
2016-09-30 11:07:28 +0200

    added available check to JSONReader

--- Sources/JSONReader.swift
+++ Sources/JSONReader.swift
@@ -2,15 +2,23 @@ import UIKit
 
 class JSONReader: NSObject {
     static func parseFile(named: String!, for bundle: Bundle) -> [String : Any]? {
+        let named = named.replacingOccurrences(of: "/", with: "_")
+
         do {
-            guard let data = NSDataAsset(name: named, bundle: bundle)?.data else {
-                return nil
+            if #available(iOS 9.0, *) {
+                guard let data = NSDataAsset(name: named, bundle: bundle)?.data else {
+                    return nil
+                }
+                return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+
+            } else {
+                print("🖕🏻 Faro json mocking only works on iOS 9")
+               return nil
             }
-            
-            return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+
         } catch {
-            PrintFaroError(FaroError.nonFaroError(error))
-            return nil
+                PrintFaroError(FaroError.nonFaroError(error))
+                return nil
         }
     }
 }
