0739833bf4ee4715cc96f33bc30579c41c56fc81
StijnWillems <stijn.willems@icapps.com>
2016-10-14 21:57:33 +0200

    param init check

---
+++ Example/Tests/ParametersSpec.swift
@@ -0,0 +1,22 @@
+import Quick
+import Nimble
+
+import Faro
+@testable import Faro_Example
+
+class ParametersSped: QuickSpec {
+
+    override func spec() {
+        describe("Parameters") {
+
+            context("fail for specific types") {
+                it("not accept non string url components"){
+                    let parameters = Parameters(type: .urlComponents, parameters: ["int": 0])
+
+                    expect(parameters).to(beNil())
+                }
+            }
+        }
+    }
+    
+}
--- Sources/Parameters.swift
+++ Sources/Parameters.swift
@@ -1,16 +1,19 @@
-//
-//  Parameters.swift
-//  Pods
-//
-//  Created by Kris Boonefaes on 20/09/16.
-//
-//
 
 public struct Parameters {
     public let type: ParameterType
     public let parameters: [String: Any]
     
-    public init(type: ParameterType, parameters: [String: Any]) {
+    public init?(type: ParameterType, parameters: [String: Any]) {
+        switch type {
+        case .urlComponents:
+            guard let _ = parameters as? [String: String] else {
+                printFaroError(FaroError.malformed(info: "url components should be only Strings"))
+                return nil
+            }
+        default:
+            break
+        }
+
         self.type = type
         self.parameters = parameters
     }
