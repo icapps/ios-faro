53c1e0c8be7d16d1647885f3d1fa3fc62398d0fa
StijnWillems <stijn.willems@icapps.com>
2016-12-23 14:40:44 +0100

    example app works

--- Example/Example/Modules/Swift/PostViewController.swift
+++ Example/Example/Modules/Swift/PostViewController.swift
@@ -10,40 +10,40 @@ class PostViewController: UIViewController {
 
         let service = ExampleService()
         let call = Call(path: "posts")
-//
-//        service.perform(call) { (result: Result<Post>) in
-//            DispatchQueue.main.async {
-//                switch result {
-//                case .models(let models):
-//                    self.label.text = "Performed call for posts"
-//                    printBreadcrumb("\(models!.map {"\($0.uuid): \($0.title!)"})")
-//                default:
-//                    printError("Could not perform call for posts")
-//                }
-//            }
-//        }
-//
-//        let serviceQueue = ExampleServiceQueue { _ in
-//            printBreadcrumb("🎉 queued call finished")
-//        }
-//
-//        let fail: (FaroError) -> () = { error in
-//            printError("An error happed: \(error)")
-//        }
-//
-//        serviceQueue.performCollection(call, autoStart: false, fail: fail) { (model: [Post]) in
-//            printBreadcrumb("Task 1 finished")
-//        }
-//
-//        serviceQueue.performCollection(call, autoStart: false, fail: fail) { (model: [Post]) in
-//            printBreadcrumb("Task 2 finished")
-//        }
-//
-//        serviceQueue.performCollection(call, autoStart: false, fail: fail) { (model: [Post]) in
-//            printBreadcrumb("Task 3 finished")
-//        }
-//
-//        serviceQueue.resumeAll()
+
+        service.perform(call) { (result: Result<Post>) in
+            DispatchQueue.main.async {
+                switch result {
+                case .models(let models):
+                    self.label.text = "Performed call for posts"
+                    printBreadcrumb("\(models!.map {"\($0.uuid): \($0.title)"})")
+                default:
+                    printError("Could not perform call for posts")
+                }
+            }
+        }
+
+        let serviceQueue = ExampleServiceQueue { _ in
+            printBreadcrumb("🎉 queued call finished")
+        }
+
+        let fail: (FaroError) -> () = { error in
+            printError("An error happed: \(error)")
+        }
+
+        serviceQueue.performCollection(call, autoStart: false, fail: fail) { (model: [Post]) in
+            printBreadcrumb("Task 1 finished")
+        }
+
+        serviceQueue.performCollection(call, autoStart: false, fail: fail) { (model: [Post]) in
+            printBreadcrumb("Task 2 finished")
+        }
+
+        serviceQueue.performCollection(call, autoStart: false, fail: fail) { (model: [Post]) in
+            printBreadcrumb("Task 3 finished")
+        }
+
+        serviceQueue.resumeAll()
     }
 
 }
