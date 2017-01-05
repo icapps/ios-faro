9c6619bf9a9382180bc805a94b2344ae035305fc
StijnWillems <stijn.willems@icapps.com>
2017-01-05 12:53:47 +0100

    💪🏼 parse rawRepresentable Strings and Ints

--- Example/Tests/DeserializeFunctionsSpec.swift
+++ Example/Tests/DeserializeFunctionsSpec.swift
@@ -4,65 +4,128 @@ import Nimble
 import Faro
 @testable import Faro_Example
 
+enum RawInt: Int {
+	case zero, one
+}
+
+enum RawString: String {
+	case first, second
+}
+
 class DeserializeFunctionSpec: QuickSpec {
 
     override func spec() {
         describe("DeserializeFunctionSpec") {
 
-            it("should parse generic value from JSON") {
-                let uuidKey = "uuid"
-                let json = [uuidKey:"some id" as Any]
-                let o1 = DeserializableObject(from: ["":""])!
+			context("should parse form JSON") {
 
-                o1.uuid = try! parse(uuidKey, from: json)
+				context("Values") {
 
-                expect(o1.uuid) == json["uuid"] as? String
-            }
+					it("Any value") {
+						let uuidKey = "uuid"
+						let json = [uuidKey:"some id" as Any]
+						let o1 = DeserializableObject(from: ["":""])!
 
-            it("should parse Date from JSON with TimeInterval") {
-                let dateKey = "date"
-                let dateTimeInterval: TimeInterval = 12345.0
-                let json = ["date": dateTimeInterval]
-                let o1 = DeserializableObject(from: ["":""])!
+						o1.uuid = try! parse(uuidKey, from: json)
 
-                o1.date = try! parse(dateKey, from: json)
+						expect(o1.uuid) == json["uuid"] as? String
+					}
 
-                let date = Date(timeIntervalSince1970: json["date"]!)
-                expect(o1.date) == date
-            }
+					context("RawRepresentable") {
 
-            it("should parse Date from JSON with String") {
-                let dateKey = "date"
-                let dateString = "1994-08-20"
-                let json = [dateKey: dateString as Any]
-                let o1 = DeserializableObject(from: ["":""])!
+						context("No throw") {
 
-                o1.date = try! parse(dateKey, from: json, format: "yyyy-MM-dd")
+							it("Int") {
+								expect {
+									let foo: RawInt = try parse("rawInt", from: ["rawInt": 0])
+									return expect(foo.rawValue) == RawInt.zero.rawValue
+									}.toNot(throwError())
+							}
 
-                expect(o1.date).toNot(beNil())
-            }
+							it("String") {
+								expect {
+									let foo: RawString = try parse("rawInt", from: ["rawInt": "first"])
+									return expect(foo.rawValue) == RawString.first.rawValue
+									}.toNot(throwError())
+							}
 
-            it("should parse generic object from JSON") {
-                let dict: [String: Any] = ["uuid":"some id"]
-                let json: [String: Any] = ["node": dict]
+						}
 
-                let o1: DeserializableObject = try! parse("node", from: json)
+						context("Throw") {
 
-                expect(o1.uuid) == "some id"
-            }
+							it("Int") {
+								expect {
+									let _: RawInt = try parse("rawInt", from: ["rawInt": 10])
+									return false
+								}.to(throwError())
+							}
 
-            it("should parse generic object arrays from JSON") {
-                let dict1 = ["uuid": "id1"]
-                let dict2 = ["uuid":"id2"]
-                let json: [String: Any] = ["node": [dict1, dict2]]
+							it("String") {
+								expect {
+									let _: RawString = try parse("rawInt", from: ["rawInt": "unknown"])
+									return false
+								}.to(throwError())
+							}
+						}
+
+					}
+
+				}
+
+				context ("Date") {
+
+					it("has TimeInterval") {
+						let dateKey = "date"
+						let dateTimeInterval: TimeInterval = 12345.0
+						let json = ["date": dateTimeInterval]
+						let o1 = DeserializableObject(from: ["":""])!
+
+						o1.date = try! parse(dateKey, from: json)
+
+						let date = Date(timeIntervalSince1970: json["date"]!)
+						expect(o1.date) == date
+					}
+
+					it("has String") {
+						let dateKey = "date"
+						let dateString = "1994-08-20"
+						let json = [dateKey: dateString as Any]
+						let o1 = DeserializableObject(from: ["":""])!
+
+						o1.date = try! parse(dateKey, from: json, format: "yyyy-MM-dd")
+
+						expect(o1.date).toNot(beNil())
+					}
+
+				}
+
+				context("Object") {
+
+					it("single") {
+						let dict: [String: Any] = ["uuid":"some id"]
+						let json: [String: Any] = ["node": dict]
+
+						let o1: DeserializableObject = try! parse("node", from: json)
+
+						expect(o1.uuid) == "some id"
+					}
+
+					it("collection") {
+						let dict1 = ["uuid": "id1"]
+						let dict2 = ["uuid":"id2"]
+						let json: [String: Any] = ["node": [dict1, dict2]]
 
 
-                let objectArray: [DeserializableObject] = try! parse("node", from: json)
+						let objectArray: [DeserializableObject] = try! parse("node", from: json)
 
-                expect(objectArray.count) == 2
-                expect(objectArray.first?.uuid) == "id1"
-                expect(objectArray.last?.uuid) == "id2"
-            }
+						expect(objectArray.count) == 2
+						expect(objectArray.first?.uuid) == "id1"
+						expect(objectArray.last?.uuid) == "id2"
+					}
+				}
+
+
+			}
 
         }
     }
