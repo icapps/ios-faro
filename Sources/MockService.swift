d7871d65b66eaeb4b3a9e4e79b1ebff625d46abb
StijnWillems <stijn.willems@icapps.com>
2017-01-10 16:14:40 +0100

    MockService throwable override

--- Sources/MockService.swift
+++ Sources/MockService.swift
@@ -12,6 +12,8 @@ open class MockService: Service {
         super.init(configuration: Configuration(baseURL: ""), faroSession: faroSession)
     }
 
+	// MARK: - Override not Throwable
+
     /// This method is overridden to return json or errors like as if we would do a network call.
     @discardableResult
     override open func performJsonResult<M : Deserializable>(_ call: Call, autoStart: Bool = true, jsonResult: @escaping (Result<M>) -> ()) -> URLSessionDataTask? {
@@ -38,15 +40,31 @@ open class MockService: Service {
         return MockURLSessionTask()
     }
 
+	// MARK: - Override Throwable
+
+	@discardableResult
 	open override func performJsonResult<M : Deserializable>(_ call: Call, autoStart: Bool, jsonResult: @escaping (Result<M>) throws -> (), throwHandler: @escaping (() throws -> ()) -> ()) throws -> URLSessionDataTask {
-		guard let task = (performJsonResult(call, autoStart: autoStart) { (result: Result<M>) in
-			throwHandler {
-				try jsonResult(result)
-			}
-		}) else  {
-			throw FaroError.couldNotCreateTask
+		if let mockDictionary = mockDictionary {
+			try jsonResult(.json(mockDictionary))
+			return MockURLSessionTask()
 		}
-		return task
+
+		guard let url = url(from: call) else {
+			let faroError = FaroError.malformed(info: "No valid url")
+			printFaroError(faroError)
+			try jsonResult(.failure(faroError))
+			return MockURLSessionTask()
+		}
+
+		guard let mockJSON = JSONReader.parseFile(named: url, for: bundle!) else {
+			let faroError = FaroError.malformed(info: "Could not find dummy file at \(url)")
+			printFaroError(faroError)
+			try jsonResult(.failure(faroError))
+			return MockURLSessionTask()
+		}
+
+		try jsonResult(.json(mockJSON))
+		return MockURLSessionTask()
 	}
 
     /// You can override this for custom behaviour
