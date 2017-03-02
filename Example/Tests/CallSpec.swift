a1c842cd7126337e78eb20e7e62b730a4cbb1543
StijnWillems <stijn.willems@icapps.com>
2017-03-02 20:04:33 +0100

    Possible to insert multiple parameters

--- Example/Tests/CallSpec.swift
+++ Example/Tests/CallSpec.swift
@@ -37,7 +37,7 @@ class CallSpec: QuickSpec {
         describe("Call .POST with parameters") {
             let expected = "path"
             let parameters: Parameter = .jsonNode(["id": "someId"])
-            let call = Call(path: expected, method: .POST, parameter: parameters)
+            let call = Call(path: expected, method: .POST, parameter: [parameters])
             let configuration = Faro.Configuration(baseURL: "http://someURL")
 
             it("should use POST method") {
@@ -112,19 +112,19 @@ class CallSpec: QuickSpec {
             let configuration = Faro.Configuration(baseURL: "http://someURL")
 
             func allHTTPHeaderFields(_ parameter: Parameter) -> [String: String] {
-                let call = Call(path: "path", parameter: parameter)
+                let call = Call(path: "path", parameter: [parameter])
                 let request = call.request(withConfiguration: configuration)
                 return request!.allHTTPHeaderFields!
             }
 
             func componentString(_ parameter: Parameter) -> String {
-                let call = Call(path: "path", parameter: parameter)
+                let call = Call(path: "path", parameter: [parameter])
                 let request = call.request(withConfiguration: configuration)
                 return request!.url!.absoluteString
             }
 
             func body(_ parameter: Parameter, method: HTTPMethod) -> Data? {
-                let call = Call(path: "path", method: method, parameter: parameter)
+                let call = Call(path: "path", method: method, parameter: [parameter])
                 let request = call.request(withConfiguration: configuration)
                 return request!.httpBody
             }
--- Sources/Call.swift
+++ Sources/Call.swift
@@ -1,113 +1,103 @@
 public enum HTTPMethod: String {
-    case GET, POST, PUT, DELETE, PATCH
+	case GET, POST, PUT, DELETE, PATCH
 }
 
 open class Call {
-    open let path: String
-    open let httpMethod: HTTPMethod
-    open var rootNode: String?
-    open var parameter: Parameter?
+	open let path: String
+	open let httpMethod: HTTPMethod
+	open var rootNode: String?
+	open var parameter: [Parameter]?
 
-    public convenience init<T: Serializable> (path: String, method: HTTPMethod = .POST, rootNode: String? = nil, serializableModel: T) {
-        self.init(path: path, method: method, rootNode: rootNode, parameter: .jsonNode(serializableModel.json))
-    }
-    /// Initializes Call to retreive object(s) from the server.
-    /// parameter rootNode: used to extract JSON in method `rootNode(from:)`.
-    public init(path: String, method: HTTPMethod = .GET, rootNode: String? = nil, parameter: Parameter? = nil) {
-        self.path = path
-        self.httpMethod = method
-        self.rootNode = rootNode
-        self.parameter = parameter
-    }
+	public convenience init<T: Serializable> (path: String, method: HTTPMethod = .POST, rootNode: String? = nil, serializableModel: T) {
+		self.init(path: path, method: method, rootNode: rootNode, parameter: [.jsonNode(serializableModel.json)])
+	}
+	/// Initializes Call to retreive object(s) from the server.
+	/// parameter rootNode: used to extract JSON in method `rootNode(from:)`.
+	public init(path: String, method: HTTPMethod = .GET, rootNode: String? = nil, parameter: [Parameter]? = nil) {
+		self.path = path
+		self.httpMethod = method
+		self.rootNode = rootNode
+		self.parameter = parameter
+	}
 
-    open func request(withConfiguration configuration: Configuration) -> URLRequest? {
+	open func request(withConfiguration configuration: Configuration) -> URLRequest? {
 		var request = URLRequest(url: URL(string: "\(configuration.baseURL)/\(path)")!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData) // uses default timeout
-        request.httpMethod = httpMethod.rawValue
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request = insertParameter(request: request)
-        return request
-    }
+		request.httpMethod = httpMethod.rawValue
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		insertParameter(request: &request)
+		return request
+	}
 
-    /// Use to begin paring at the correct `rootnode`.
-    /// Override if you want different behaviour then: 
-    /// `{"rootNode": <Any Node>}` `<Any Node> is returned when `rootNode` is set.
-    open func rootNode(from json: Any) -> JsonNode {
-        let json = extractNodeIfNeeded(from: json)
+	/// Use to begin paring at the correct `rootnode`.
+	/// Override if you want different behaviour then:
+	/// `{"rootNode": <Any Node>}` `<Any Node> is returned when `rootNode` is set.
+	open func rootNode(from json: Any) -> JsonNode {
+		let json = extractNodeIfNeeded(from: json)
 
-        if let jsonArray = json as? [Any] {
-            return .nodeArray(jsonArray)
-        }else if let json = json as? [String: Any] {
-            return .nodeObject(json)
-        }else {
-            return .nodeNotFound(json: json ?? "")
-        }
-    }
+		if let jsonArray = json as? [Any] {
+			return .nodeArray(jsonArray)
+		}else if let json = json as? [String: Any] {
+			return .nodeObject(json)
+		}else {
+			return .nodeNotFound(json: json ?? "")
+		}
+	}
 
-    private func extractNodeIfNeeded(from json: Any?) -> Any? {
-        guard  let rootNode = rootNode, let rootedJson = json as? [String: Any] else {
-            return json
-        }
+	open func insertParameter(request: inout URLRequest) {
+		parameter?.forEach {
+			do {
+				switch $0 {
+				case .httpHeader(let headers):
+					insertInHeaders(with: headers, request: &request)
+				case .urlComponents(let components):
+					insertInUrl(with: components, request: &request)
+				case .jsonNode(let json):
+					try insertInBody(with: json, request: &request)
+				case .jsonArray(let jsonArray):
+					try insertInBody(with: jsonArray, request: &request)
+				}
+			} catch {
+				printFaroError(error)
+			}
+		}
+	}
 
-        return rootedJson[rootNode]
-    }
-    
-    private func insertParameter(request: URLRequest) -> URLRequest {
-        guard let parameter = parameter else {
-            return request
-        }
-        
-        do {
-            switch parameter {
-            case .httpHeader(let headers):
-                return insertInHeaders(with: headers, request: request)
-            case .urlComponents(let components):
-                return insertInUrl(with: components, request: request)
-            case .jsonNode(let json):
-                return try insertInBody(with: json, request: request)
-            case .jsonArray(let jsonArray):
-                return try insertInBody(with: jsonArray, request: request)
-            }
-        } catch {
-            printFaroError(error)
-            return request
-        }
-    }
-    
-    private func insertInHeaders(with headers: [String: String], request: URLRequest) -> URLRequest {
-        var newRequest = request
-        for (key, value) in headers {
-            newRequest.addValue(value, forHTTPHeaderField: key)
-        }
-        return newRequest
-    }
-    
-    private func insertInUrl(with componentsDict: [String: String], request: URLRequest) -> URLRequest {
-        guard componentsDict.values.count > 0 else {
-            return request
-        }
+	private func extractNodeIfNeeded(from json: Any?) -> Any? {
+		guard  let rootNode = rootNode, let rootedJson = json as? [String: Any] else {
+			return json
+		}
 
-        var newRequest: URLRequest! = request
-        var components = URLComponents(url: newRequest.url!, resolvingAgainstBaseURL: false)
-        if (components?.queryItems == nil) {
-            components?.queryItems = [URLQueryItem]()
-        }
-        let sortedComponents = componentsDict.sorted(by: { $0.0 < $1.0 })
-        for (key, value) in sortedComponents {
-            components?.queryItems?.append(URLQueryItem(name: key, value: value))
-        }
-        newRequest.url = components?.url
-        return newRequest
-    }
-    
-    private func insertInBody(with json: Any, request: URLRequest) throws -> URLRequest {
-        if request.httpMethod == HTTPMethod.GET.rawValue {
-            throw FaroError.malformed(info: "HTTP " + request.httpMethod! + " request can't have a body")
-        }
-        var newRequest = request
-        newRequest.httpBody = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+		return rootedJson[rootNode]
+	}
 
-        return newRequest
-    }
+	private func insertInHeaders(with headers: [String: String], request: inout URLRequest) {
+		for (key, value) in headers {
+			request.addValue(value, forHTTPHeaderField: key)
+		}
+	}
+
+	private func insertInUrl(with componentsDict: [String: String], request: inout URLRequest) {
+		guard componentsDict.values.count > 0 else {
+			return
+		}
+
+		var components = URLComponents(url: request.url!, resolvingAgainstBaseURL: false)
+		if (components?.queryItems == nil) {
+			components?.queryItems = [URLQueryItem]()
+		}
+		let sortedComponents = componentsDict.sorted(by: { $0.0 < $1.0 })
+		for (key, value) in sortedComponents {
+			components?.queryItems?.append(URLQueryItem(name: key, value: value))
+		}
+		request.url = components?.url
+	}
+
+	private func insertInBody(with json: Any, request: inout URLRequest) throws {
+		if request.httpMethod == HTTPMethod.GET.rawValue {
+			throw FaroError.malformed(info: "HTTP " + request.httpMethod! + " request can't have a body")
+		}
+		request.httpBody = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+	}
 
 }
 
