6677efdd2903b4248b29173b5df26a293eb06585
StijnWillems <stijn.willems@icapps.com>
2017-03-03 18:12:24 +0100

    Convenience function to result

--- Sources/Service/Result.swift
+++ Sources/Service/Result.swift
@@ -7,6 +7,43 @@ public enum Result<M: Deserializable> {
     /// Server returned with statuscode 200...201 but no response data. For Example Post
     case ok
     case failure(FaroError)
+
+	public var error: FaroError? {
+		switch self {
+		case .failure(let error):
+			return error
+		default:
+			return nil
+		}
+	}
+
+	public var model: M? {
+		switch self {
+		case .model(let model):
+			return model
+		default:
+			return nil
+		}
+	}
+
+	public var models: [M]? {
+		switch self {
+		case .models(let model):
+			return model
+		default:
+			return nil
+		}
+	}
+
+	public var json: [String: Any]? {
+		switch self {
+		case .json(let raw):
+			return raw as? [String: Any]
+		default:
+			return nil
+		}
+	}
+
 }
 
 public enum WriteResult {
