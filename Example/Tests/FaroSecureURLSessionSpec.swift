6a59e34eb9093b405bc2fb992e9a5a7fd3a53661
StijnWillems <stijn.willems@icapps.com>
2017-03-06 15:43:24 +0100

    made the problem sync, should be async
    The reason for the inout is that the hashValue of the request should not change.

--- Example/Tests/FaroSecureURLSessionSpec.swift
+++ Example/Tests/FaroSecureURLSessionSpec.swift
@@ -12,7 +12,12 @@ class RetryFaroSecureURLSession: FaroSecureURLSession, HTTPURLResponseRetryable 
 		return true
 	}
 
-	func makeRequestValidforRetry(_ request: inout URLRequest, after httpResponse: HTTPURLResponse, retryCount: Int) throws {
+	func makeRequestValidforRetry(failedRequest: inout URLRequest,
+	                              after httpResponse: HTTPURLResponse,
+	                              retryCount: Int,
+	                              requestForRetry: @escaping (inout URLRequest) -> Void,
+	                              noRetryNeeded: @escaping (FaroError?) -> Void) {
+		requestForRetry(&failedRequest)
 	}
 
 }
@@ -23,8 +28,12 @@ enum RetryError: Error {
 
 class RetryStoppedFaroSecureURLSession: RetryFaroSecureURLSession {
 
-	override func makeRequestValidforRetry(_ request: inout URLRequest, after httpResponse: HTTPURLResponse, retryCount: Int) throws {
-		throw RetryError.stop
+	override func makeRequestValidforRetry(failedRequest: inout URLRequest,
+	                                       after httpResponse: HTTPURLResponse,
+	                                       retryCount: Int,
+	                                       requestForRetry: @escaping (inout URLRequest) -> Void,
+	                                       noRetryNeeded: @escaping (FaroError?) -> Void) {
+		noRetryNeeded(nil)
 	}
 
 }
@@ -36,42 +45,45 @@ class FaroSecureURLSessionSpec: QuickSpec {
 		describe("Keep track of the retry count") {
 
 			var session: RetryFaroSecureURLSession!
-			var testRequest: URLRequest!
+			var testRequest: URLRequest?
 			var httpResponse: HTTPURLResponse!
 
 			beforeEach {
 				session = RetryFaroSecureURLSession(urlSessionDelegate: FaroURLSessionDelegate(allowUntrustedCertificates: false))
 				testRequest = URLRequest(url: URL(string: "http://www.google.com")!)
-				httpResponse =  HTTPURLResponse(url: testRequest.url!, statusCode: 401, httpVersion:nil, headerFields: nil)!
+				httpResponse =  HTTPURLResponse(url: testRequest!.url!, statusCode: 401, httpVersion:nil, headerFields: nil)!
 			}
 
-			it("should retry and count") {
+			fit("should retry and count") {
 
 				expect(session.shouldRetry(httpResponse)) == true
 				expect(session.retryCountTuples.map {$0.count}) == []
 
-				var variableRequest = testRequest
-
-				try? session.makeRequestValidforRetry(&variableRequest!, after: httpResponse, retryCount: session.retryCount(for: testRequest))
+				session.handleRetry(data: nil, httpResponse: httpResponse, for: &testRequest!, completionHandler: {(_, _, _) in }, task: { (_) in
+				}, noRetryNeeded: {_ in})
 
 				expect(session.retryCountTuples.map {$0.count}) == [1]
 			}
 
 			context("has tried once") {
 				beforeEach {
-					let _ = session.handleRetry(data:nil, httpResponse: httpResponse, for: testRequest, completionHandler: {(_, _, _) in })
+					session.handleRetry(data: nil, httpResponse: httpResponse, for: &testRequest!, completionHandler: {(_, _, _) in }, task: { (task) in
+					}, noRetryNeeded: { (_) in
+					})
 
 					expect(session.retryCountTuples.map {$0.count}) == [1]
 				}
 
 				it("count should increase a second time") {
-					let _ = session.handleRetry(data:nil, httpResponse: httpResponse, for: testRequest, completionHandler: {(_, _, _) in })
+					session.handleRetry(data: nil, httpResponse: httpResponse, for: &testRequest!, completionHandler: {(_, _, _) in }, task: { (task) in
+					}, noRetryNeeded: { (_) in
+					})
 
 					expect(session.retryCountTuples.map {$0.count}) == [2]
 				}
 
 				it("removes request from retryCount when finished") {
-					session.handleEnding(for: testRequest)
+					session.handleEnding(for: &testRequest!)
 
 					expect(session.retryCountTuples.map {$0.count}) == []
 				}
@@ -79,7 +91,7 @@ class FaroSecureURLSessionSpec: QuickSpec {
 				it("still contains other retry counts after one is stopped") {
 					session.retryCountTuples.append((hashValue: 169, count: 10))
 
-					session.handleEnding(for: testRequest)
+					session.handleEnding(for: &testRequest!)
 
 					expect(session.retryCountTuples.map {$0.hashValue}) == [169]
 				}
@@ -90,29 +102,31 @@ class FaroSecureURLSessionSpec: QuickSpec {
 
 		describe("Stop retrying") {
 			var session: RetryStoppedFaroSecureURLSession!
-			var testRequest: URLRequest!
+			var testRequest: URLRequest?
 			var httpResponse: HTTPURLResponse!
 
 			beforeEach {
 				session = RetryStoppedFaroSecureURLSession(urlSessionDelegate: FaroURLSessionDelegate(allowUntrustedCertificates: false))
 				testRequest = URLRequest(url: URL(string: "http://www.google.com")!)
-				session.retryCountTuples.append((hashValue: testRequest.hashValue, count: 1))
-				httpResponse =  HTTPURLResponse(url: testRequest.url!, statusCode: 401, httpVersion:nil, headerFields: nil)!
+				session.retryCountTuples.append((hashValue: testRequest!.hashValue, count: 1))
+				httpResponse =  HTTPURLResponse(url: testRequest!.url!, statusCode: 401, httpVersion:nil, headerFields: nil)!
 			}
 
 			it("has request in retry") {
-				expect(session.retryCountTuples.map {$0.hashValue}) == [testRequest.hashValue]
+				expect(session.retryCountTuples.map {$0.hashValue}) == [testRequest!.hashValue]
 			}
 
 			it("removes request when stopped") {
-				session.handleEnding(for: testRequest)
+				session.handleEnding(for: &testRequest!)
 				expect(session.retryCountTuples.map {$0.hashValue}) == []
 			}
 
 			it("still contains other retry counts after one is stopped") {
 				session.retryCountTuples.append((hashValue: 169, count: 10))
 
-				let _ = session.handleRetry(data: nil, httpResponse: httpResponse, for: testRequest, completionHandler: { (_, _, _) in})
+				session.handleRetry(data: nil, httpResponse: httpResponse, for: &testRequest!, completionHandler: {(_, _, _) in }, task: { (task) in
+				}, noRetryNeeded: { (_) in
+				})
 
 				expect(session.retryCountTuples.map {$0.hashValue}) == [169]
 			}
