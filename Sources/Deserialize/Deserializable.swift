820716a7aa8e61dd5ef408d5a986099c94a9697c
StijnWillems <stijn.willems@icapps.com>
2017-04-20 11:30:36 +0200

    Deserialize operators renamed

--- Sources/Deserialize/Deserializable.swift
+++ Sources/Deserialize/Deserializable.swift
@@ -3,12 +3,23 @@ import Foundation
 /// Sets data on a class `Type`.
 public protocol Deserializable {
 
-    init?(from raw: Any)
+	/// To initialize you can use the `DeserializeCreateFunctions`
+	/// You should return nil if `raw` contain not all required keys to instantiate.
+	init?(from raw: Any)
 
 }
 
 public protocol Updatable {
 
-    func update(from raw: Any) throws
+	/// To update you can use the `DeserializeUpdateCreateOperators`
+	/// The operators make sure you update the memory location of an existing instance rather the asigning a different location in memory.
+	func update(from raw: Any) throws
 
+}
+
+/// Impliment to perform deserialization on linked object via the value of key.
+public protocol Linkable {
+	associatedtype ValueType
+
+	var link: (key: String, value: ValueType) {get}
 }
