15e48c65fe308285d7ae58dc0aba8a0a9276b700
StijnWillems <stijn.willems@icapps.com>
2017-05-22 11:40:04 +0200

    Parameter logic and value convenience stuff

--- Sources/Request/ParameterEnum.swift
+++ Sources/Request/ParameterEnum.swift
@@ -28,4 +28,81 @@ public enum Parameter: CustomDebugStringConvertible {
 			return "\(components.map {(key:$0.key, value: $0.value)}.reduce("• .urlComponents:", {"\($0)\n• \($1)"}))"
 		}
 	}
+
+	// MARK: - Logic
+
+	public var isHeader: Bool {
+		switch self {
+		case .httpHeader(_):
+			return true
+		default:
+			return false
+		}
+	}
+
+	public var isJSONArray: Bool {
+		switch self {
+		case .jsonArray(_):
+			return true
+		default:
+			return false
+		}
+	}
+
+	public var isJSONNode: Bool {
+		switch self {
+		case .jsonNode(_):
+			return true
+		default:
+			return false
+		}
+	}
+
+	public var isUrlComponents: Bool {
+		switch self {
+		case .urlComponents(_):
+			return true
+		default:
+			return false
+		}
+	}
+
+	// MARK: - Values
+
+	public var httpHeaderValue: [String: String]? {
+		switch self {
+		case .httpHeader(let header):
+			return header
+		default:
+			return nil
+		}
+	}
+
+	public var jsonArrayValue: [[String: Any]]? {
+		switch self {
+		case .jsonArray(let json):
+			return json
+		default:
+			return nil
+		}
+	}
+
+	public var jsonNodeValue: [String: Any]? {
+		switch self {
+		case .jsonNode(let node):
+			return node
+		default:
+			return nil
+		}
+	}
+
+	public var urlComponentsValue: [String: String]? {
+		switch self {
+		case .urlComponents(let components):
+			return components
+		default:
+			return nil
+		}
+	}
+	
 }
