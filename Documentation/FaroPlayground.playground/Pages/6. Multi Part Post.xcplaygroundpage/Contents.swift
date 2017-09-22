ccdc387cf9b923da5dcb8f853716d56bf126a0b3
StijnWillems <stijn.willems@icapps.com>
2017-09-22 22:43:33 +0200

    multi part post

--- Documentation/FaroPlayground.playground/Pages/6. Multi Part Post.xcplaygroundpage/Contents.swift
+++ Documentation/FaroPlayground.playground/Pages/6. Multi Part Post.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
+//: [Table of Contents](0.%20Table%20of%20Contents)   [Previous](@previous) / [Next](@next)
+import Faro
+import UIKit
+//: # Multi Part Post
+//: You can use `Faro` to send a `multipart/form-data` to a server. To use this, you add the multipart file as a parameter to the `Call`.=
+//: *Example*
 
-import Foundation
+let image = UIImage(named: "Tom.jpg")!
+let jpeg = UIImageJPEGRepresentation(image, 0.7)!
 
-var str = "Hello, playground"
+//: Create a multipart object and add it to the call
+let multipart = MultipartFile(parameterName: "image",
+                              data: jpeg, mimeType: .jpeg)
+let call = Call(path: "queries",
+                method: .POST,
+                parameter: [.multipart(multipart)])
 
-//: [Next](@next)
+//: Again provide a fake response:
+let configuration = Configuration(baseURL: "http://www.yourServer.com")
+let response = HTTPURLResponse(url: configuration.baseURL!, statusCode: 200, httpVersion: nil, headerFields: nil)
+let session = MockSession(data: nil, urlResponse: response, error: nil)
+
+let service = Service(call: call, configuration: configuration, faroSession: session)
+
+service.perform(Service.NoResponseData.self){
+    do {
+        _ = try $0()
+        print("Multipart post succes")
+    } catch {
+        print(error)
+    }
+}
+//: > We do not have a service to send this request to. So in this case the mock is a bit to rough. We do not realy test if the multipart post is handeled correctly. But you get the idea.
+//: [Table of Contents](0.%20Table%20of%20Contents)   [Previous](@previous) / [Next](@next)
