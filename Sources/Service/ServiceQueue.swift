0e614c10f6014684cdbceb3033b46ede395f86d5
Ben Algoet <ben.algoet@icapps.com>
2017-10-30 11:05:19 +0100

    Return errors for queue

--- Sources/Service/ServiceQueue.swift
+++ Sources/Service/ServiceQueue.swift
@@ -5,25 +5,26 @@ import Foundation
 /// It has its own `URLSession` which it invalidates once the queue is finished. You need to create another instance of `ServiceQueue` to be able to
 /// perform new request after you fired the first queue.
 open class ServiceQueue: Service {
-
+    
     var taskQueue: Set<URLSessionDataTask>
     var failedTasks: Set<URLSessionTask>?
-
-    private let final: (_ failedTasks: Set<URLSessionTask>?)->()
-
+    lazy var errors: [FaroError]? = [FaroError]()
+    
+    private let final: (_ failedTasks: Set<URLSessionTask>?, _ errors:[FaroError]?)->()
+    
     /// Creates a queue that lasts until final is called. When all request in the queue are finished the session becomes invalid.
     /// For future queued request you have to create a new ServiceQueue instance.
     /// - parameter configuration: Faro service configuration
     /// - parameter faroSession: You can provide a custom `URLSession` via `FaroQueueSession`.
     /// - parameter final: closure is callen when all requests are performed.
-    public init(configuration: Configuration, faroSession: FaroQueueSessionable = FaroQueueSession(), final: @escaping(_ failedTasks: Set<URLSessionTask>?)->()) {
+    public init(configuration: Configuration, faroSession: FaroQueueSessionable = FaroQueueSession(), final: @escaping(_ failedTasks: Set<URLSessionTask>?, _ errors: [FaroError]?)->()) {
         self.final = final
         taskQueue = Set<URLSessionDataTask>()
         super.init(configuration: configuration, faroSession: faroSession)
     }
-
-	// MARK: - Override not throwing
-
+    
+    // MARK: - Override not throwing
+    
     open override func performJsonResult<M: Deserializable>(_ call: Call, autoStart: Bool = false, jsonResult: @escaping (Result<M>) -> ()) -> URLSessionDataTask? {
         var task: URLSessionDataTask?
         task = super.performJsonResult(call, autoStart: autoStart) { [weak self] (stage1JsonResult: Result<M>) in
@@ -35,21 +36,21 @@ open class ServiceQueue: Service {
             switch stage1JsonResult {
             case .json(_), .ok:
                 strongSelf.cleanupQueue(for: task)
-            case .failure(_):
-                strongSelf.cleanupQueue(for: task, didFail: true)
+            case .failure(let error):
+                strongSelf.cleanupQueue(for: task, with: error, didFail: true)
             default:
                 strongSelf.cleanupQueue(for: task)
             }
-
-
+            
+            
             jsonResult(stage1JsonResult)
             strongSelf.shouldCallFinal()
         }
-
+        
         add(task)
         return task
     }
-
+    
     open override func performWrite(_ writeCall: Call, autoStart: Bool, writeResult: @escaping (WriteResult) -> ()) -> URLSessionDataTask? {
         var task: URLSessionDataTask?
         task = super.performWrite(writeCall, autoStart: autoStart) { [weak self] (result) in
@@ -57,71 +58,71 @@ open class ServiceQueue: Service {
                 writeResult(result)
                 return
             }
-
+            
             switch result {
             case .ok:
                 strongSelf.cleanupQueue(for: task)
-            default:
-                strongSelf.cleanupQueue(for: task, didFail: true)
+            case .failure(let error):
+                strongSelf.cleanupQueue(for: task, with: error, didFail: true)
             }
-
+            
             writeResult(result)
             strongSelf.shouldCallFinal()
         }
         add(task)
         return task
     }
-
-	// MARK: - Override Throwing
-
-	open override func performJsonResult<M : Deserializable>(_ call: Call, autoStart: Bool, jsonResult: @escaping (Result<M>) throws -> (), throwHandler: @escaping (() throws -> ()) -> ()) throws -> URLSessionDataTask {
-		var task: URLSessionDataTask!
-		task = try super.performJsonResult(call, autoStart: autoStart, jsonResult: { [weak self] (stage1JsonResult: Result<M>) in
-			guard let strongSelf = self else {
-				try jsonResult(stage1JsonResult)
-				return
-			}
-			/// Store ID of failed tasks to report
-			switch stage1JsonResult {
-			case .json(_), .ok:
-				strongSelf.cleanupQueue(for: task)
-			case .failure(_):
-				strongSelf.cleanupQueue(for: task, didFail: true)
-			default:
-				strongSelf.cleanupQueue(for: task)
-			}
-
-
-			try jsonResult(stage1JsonResult)
-			strongSelf.shouldCallFinal()
-		}, throwHandler: throwHandler)
-
-		add(task)
-		return task
-	}
-	open override func performWrite(_ writeCall: Call, autoStart: Bool = true, writeResult: @escaping (WriteResult) throws -> (), throwHandler: @escaping (()throws ->()) -> () = faroDefaultThrowHandler) throws -> URLSessionDataTask {
-		var task: URLSessionDataTask!
-		task = try super.performWrite(writeCall, autoStart: autoStart, writeResult: { [weak self] (result) in
-			guard let strongSelf = self else {
-				try writeResult(result)
-				return
-			}
-
-			switch result {
-			case .ok:
-				strongSelf.cleanupQueue(for: task)
-			default:
-				strongSelf.cleanupQueue(for: task, didFail: true)
-			}
-
-			try writeResult(result)
-			strongSelf.shouldCallFinal()
-		}, throwHandler: throwHandler)
-		add(task)
-		return task
-	}
-	
-	// MARK: - Private
+    
+    // MARK: - Override Throwing
+    
+    open override func performJsonResult<M : Deserializable>(_ call: Call, autoStart: Bool, jsonResult: @escaping (Result<M>) throws -> (), throwHandler: @escaping (() throws -> ()) -> ()) throws -> URLSessionDataTask {
+        var task: URLSessionDataTask!
+        task = try super.performJsonResult(call, autoStart: autoStart, jsonResult: { [weak self] (stage1JsonResult: Result<M>) in
+            guard let strongSelf = self else {
+                try jsonResult(stage1JsonResult)
+                return
+            }
+            /// Store ID of failed tasks to report
+            switch stage1JsonResult {
+            case .json(_), .ok:
+                strongSelf.cleanupQueue(for: task)
+            case .failure(let error):
+                strongSelf.cleanupQueue(for: task, with: error, didFail: true)
+            default:
+                strongSelf.cleanupQueue(for: task)
+            }
+            
+            
+            try jsonResult(stage1JsonResult)
+            strongSelf.shouldCallFinal()
+            }, throwHandler: throwHandler)
+        
+        add(task)
+        return task
+    }
+    open override func performWrite(_ writeCall: Call, autoStart: Bool = true, writeResult: @escaping (WriteResult) throws -> (), throwHandler: @escaping (()throws ->()) -> () = faroDefaultThrowHandler) throws -> URLSessionDataTask {
+        var task: URLSessionDataTask!
+        task = try super.performWrite(writeCall, autoStart: autoStart, writeResult: { [weak self] (result) in
+            guard let strongSelf = self else {
+                try writeResult(result)
+                return
+            }
+            
+            switch result {
+            case .ok:
+                strongSelf.cleanupQueue(for: task)
+            case .failure(let error):
+                strongSelf.cleanupQueue(for: task, with: error, didFail: true)
+            }
+            
+            try writeResult(result)
+            strongSelf.shouldCallFinal()
+            }, throwHandler: throwHandler)
+        add(task)
+        return task
+    }
+    
+    // MARK: - Private
     private func add(_ task: URLSessionDataTask?) {
         guard let createdTask = task else {
             printFaroError(FaroError.invalidSession(message: "\(self) tried to "))
@@ -129,8 +130,8 @@ open class ServiceQueue: Service {
         }
         taskQueue.insert(createdTask)
     }
-
-    private func cleanupQueue(for task: URLSessionDataTask?, didFail: Bool = false) {
+    
+    private func cleanupQueue(for task: URLSessionDataTask?, with error: FaroError? = nil, didFail: Bool = false) {
         if let task = task {
             let _ = taskQueue.remove(task)
             if(didFail) {
@@ -138,44 +139,49 @@ open class ServiceQueue: Service {
                     failedTasks = Set<URLSessionTask>()
                 }
                 failedTasks?.insert(task)
+                
+                guard let error = error else {
+                    return
+                }
+                errors?.append(error)
             }
         }
     }
-
+    
     private func shouldCallFinal() {
         if !hasOustandingTasks {
-            final(failedTasks)
+            final(failedTasks, errors)
             finishTasksAndInvalidate()
         }
     }
-
+    
     // MARK: - Interact with tasks
-
+    
     open var hasOustandingTasks: Bool {
         get {
             return taskQueue.count > 0
         }
     }
-
+    
     open func resume(_ task: URLSessionDataTask) {
         faroSession.resume(task)
     }
-
+    
     open func resumeAll() {
         let notStartedTasks = taskQueue.filter { $0.state != .running || $0.state != .completed}
         notStartedTasks.forEach { (task) in
             faroSession.resume(task)
         }
     }
-
+    
     // MARK: - Invalidate session overrides
-
+    
     override open func invalidateAndCancel() {
         taskQueue.removeAll()
         failedTasks?.removeAll()
         faroSession.invalidateAndCancel()
     }
-
+    
     deinit {
         faroSession.finishTasksAndInvalidate()
     }
