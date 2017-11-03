4131fa167faf80e83f724b8201553189ba35a9fb
StijnWillems <stijn.willems@icapps.com>
2017-11-03 15:08:40 +0100

    writing stubbed

--- Documentation/FaroPlayground.playground/Pages/5. Write to the service.xcplaygroundpage/Contents.swift
+++ Documentation/FaroPlayground.playground/Pages/5. Write to the service.xcplaygroundpage/Contents.swift
@@ -23,17 +23,15 @@ struct Product: Encodable {
         return try! JSONSerialization.jsonObject(with: data) as? [String: Any]
     }
 }
-//: Service mocking via session
-let configuration = Configuration(baseURL: "http://www.yourServer.com")
-//: > *Tip:* Try changing the statusCode to see failure handling
-let statusCode = 200
-let response = HTTPURLResponse(url: configuration.baseURL!, statusCode: statusCode, httpVersion: nil, headerFields: nil)
-let session = MockSession(data: nil, urlResponse: response, error: nil)
+
 //: Product encoding
 let product = Product(name: "Melon", points: 100)
 let parameters = [Parameter.jsonNode(product.jsonDict!)]
 let call = Call(path: "products", method: .POST, parameter: parameters)
-let service = Service(call: call, configuration: configuration, faroSession: session)
+let service = StubService(call: call)
+
+call.stub(statusCode: 200, body: ["":""])
+
 //: Use `Service.NoResponseData.self` as the type of the response. This is to allow no response data.
 service.perform(Service.NoResponseData.self) { postSuccess in
     do {
